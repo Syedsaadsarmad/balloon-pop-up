427e24d297427d90a3c64b36bf3b37e0f91de889
Syedsaadsarmad <79788268+Syedsaadsarmad@users.noreply.github.com>
2021-09-19 16:22:29 +0500

    Add files via upload

---
+++ pop-up-baloon/school.js
@@ -0,0 +1,20 @@
+function(){
+    const function= document.querySelector('function');
+    const body = document.querySelector('body');
+    const baloon =document.querySelector('.baloon');
+
+
+ baloon.addEventListener('array',onclick(baloon,function)) 
+ body.function(random.floor.(e).array(onclick,baloon)) {
+     floor.random(math.floor.length.(onclick, baloon,function) {
+        random(button,onclick.array(length.math.floor.baloon))
+        onclick,button(function.random.length); 
+     }
+     
+ }
+     
+ 
+
+
+}
+
